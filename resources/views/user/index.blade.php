3acba7ec59252a62c4a1e8406c231588c61f466f
Vannchhai POV <vannchhai@MacBooKs-MacBook-Pro.local>
2018-01-22 18:52:16 +0700

    public user datatable

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -80,14 +80,14 @@
 @endsection
 
 @section('javascript')
-    <script src="{{ asset('plugins/jquery-datatable/jquery.dataTables.js')}}"></script>
-    <script src="{{ asset('plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js')}}"></script>
-    <script src="{{ asset('plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js')}}"></script>
-    <script src="{{ asset('plugins/jquery-datatable/extensions/export/buttons.flash.min.js')}}"></script>
-    <script src="{{ asset('plugins/jquery-datatable/extensions/export/jszip.min.js')}}"></script>
-    <script src="{{ asset('plugins/jquery-datatable/extensions/export/pdfmake.min.js')}}"></script>
-    <script src="{{ asset('plugins/jquery-datatable/extensions/export/vfs_fonts.js')}}"></script>
-    <script src="{{ asset('plugins/jquery-datatable/extensions/export/buttons.html5.min.js')}}"></script>
-    <script src="{{ asset('plugins/jquery-datatable/extensions/export/buttons.print.min.js')}}"></script>
-    <script src="{{ asset('js/pages/tables/jquery-datatable.js')}}"></script>
+    <script src="{{ asset('public/plugins/jquery-datatable/jquery.dataTables.js')}}"></script>
+    <script src="{{ asset('public/plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js')}}"></script>
+    <script src="{{ asset('public/plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js')}}"></script>
+    <script src="{{ asset('public/plugins/jquery-datatable/extensions/export/buttons.flash.min.js')}}"></script>
+    <script src="{{ asset('public/plugins/jquery-datatable/extensions/export/jszip.min.js')}}"></script>
+    <script src="{{ asset('public/plugins/jquery-datatable/extensions/export/pdfmake.min.js')}}"></script>
+    <script src="{{ asset('public/plugins/jquery-datatable/extensions/export/vfs_fonts.js')}}"></script>
+    <script src="{{ asset('public/plugins/jquery-datatable/extensions/export/buttons.html5.min.js')}}"></script>
+    <script src="{{ asset('public/plugins/jquery-datatable/extensions/export/buttons.print.min.js')}}"></script>
+    <script src="{{ asset('public/js/pages/tables/jquery-datatable.js')}}"></script>
 @endsection
