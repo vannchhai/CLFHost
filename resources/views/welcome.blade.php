2f474e57e4aee81f08327585a38ce8ddd299480c
Vannchhai POV <vannchhai@MacBooKs-MacBook-Pro.local>
2018-01-24 12:01:23 +0700

    push resource image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -305,14 +305,14 @@
                                 <div class="row">
                                     <div class="col-md-2">
                                         <div class="review-person">
-                                            <img src="http://api.randomuser.me/portraits/women/94.jpg" alt="" class="img-responsive">
+                                            <img src="{{ url('public/assets/img/freeze/user/mesa.jpg')}}" alt="" class="img-responsive">
                                         </div>
                                     </div>
                                     <div class="col-md-10">
                                         <div class="review-comment">
-                                            <h3>“I love Oleose, I highly rfreezemmend it, Everyone Try It Now”</h3>
+                                            <h3>“I love This App, I highly rfreezemmend it, Everyone Try It Now”</h3>
                                             <p>
-                                                - Krin Fox
+                                                - Mesa
                                                 <span>
                                                     <i class="fa fa-star fa-lg"></i>
                                                     <i class="fa fa-star fa-lg"></i>
@@ -329,14 +329,14 @@
                                 <div class="row">
                                     <div class="col-md-2">
                                         <div class="review-person">
-                                            <img src="http://api.randomuser.me/portraits/men/70.jpg" alt="" class="img-responsive">
+                                            <img src="{{ url('public/assets/img/freeze/user/viphea.jpg')}}" alt="" class="img-responsive">
                                         </div>
                                     </div>
                                     <div class="col-md-10">
                                         <div class="review-comment">
                                             <h3>“Oleaose Is The Best Stable, Fast App I Have Ever Experienced”</h3>
                                             <p>
-                                                - Theodore Willis
+                                                - Viphea
                                                 <span>
                                                     <i class="fa fa-star fa-lg"></i>
                                                     <i class="fa fa-star fa-lg"></i>
@@ -353,14 +353,38 @@
                                 <div class="row">
                                     <div class="col-md-2">
                                         <div class="review-person">
-                                            <img src="http://api.randomuser.me/portraits/men/93.jpg" alt="" class="img-responsive">
+                                            <img src="{{ url('public/assets/img/freeze/user/voleak.jpg')}}" alt="" class="img-responsive">
                                         </div>
                                     </div>
                                     <div class="col-md-10">
                                         <div class="review-comment">
                                             <h3>“Keep It Up Guys Your Work Rules, Cheers :)”</h3>
                                             <p>
-                                                - Ricky Grant
+                                                - Voleak
+                                                <span>
+                                                    <i class="fa fa-star fa-lg"></i>
+                                                    <i class="fa fa-star fa-lg"></i>
+                                                    <i class="fa fa-star fa-lg"></i>
+                                                    <i class="fa fa-star-half-o fa-lg"></i>
+                                                    <i class="fa fa-star-o fa-lg"></i>
+                                                </span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="review rollitin">
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <div class="review-person">
+                                            <img src="{{ url('public/assets/img/freeze/user/chenda.jpg')}}" alt="" class="img-responsive">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-10">
+                                        <div class="review-comment">
+                                            <h3>“Nice Projection Application”</h3>
+                                            <p>
+                                                - Chenda
                                                 <span>
                                                     <i class="fa fa-star fa-lg"></i>
                                                     <i class="fa fa-star fa-lg"></i>
