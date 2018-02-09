ebb48c4b0fe420a2bcd5e014906c2968465c6f8e
Vannchhai POV <vannchhai@MacBooKs-MacBook-Pro-2.local>
2018-02-09 21:44:42 +0700

    upload size

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -124,8 +124,8 @@ class ApiController extends FrontController
             $imgItm = file_put_contents($path,base64_decode($data));
             $destination = public_path().'/assets/upload/user_'.$insertItem->user_id.'/_thumnail/';
 
-            $width = 200;
-            $height = 200;
+            $width = 400;
+            $height = 400;
 
             if (!file_exists($destination)) {
                 mkdir($destination, 0777, true);
